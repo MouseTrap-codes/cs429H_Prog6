027fcd88e4274627d9ac2f9fd4490dcc7f69026f
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-02-21 05:18:29 -0600

    a

--- main.c
+++ main.c
@@ -551,12 +551,25 @@ int main(int argc, char *argv[]) {
         
         // For immediate instructions:
         // For brr L (opcode 0xA) we sign-extend the immediate since it can be negative.
+        // For immediate instructions:
         if (opcode == 0xA) {
+            // brr L needs sign extension
             int64_t signedImm = imm;
             if (imm & 0x800) // If bit 11 is set, sign-extend.
                 signedImm |= ~0xFFF;
-            L = (uint64_t)signedImm;
-        } else if (opcode == 0x19 || opcode == 0x1B || opcode == 0x12 || opcode == 0xf || opcode == 0x5 || opcode == 0x7) {
+            L = (uint64_t) signedImm;
+
+        } else if (
+            // any opcode that uses bits [11:0] as an unsigned immediate
+            opcode == 0x19 || // addi
+            opcode == 0x1B || // subi
+            opcode == 0x12 || // mov rd, L
+            opcode == 0xF  || // priv rd, rs, rt, L
+            opcode == 0x5  || // shftri
+            opcode == 0x7  || // shftli
+            opcode == 0x10 || // mov rd, (rs)(L)  <--- Add this
+            opcode == 0x13    // mov (rd)(L), rs  <--- And this
+        ) {
             L = imm;
         }
         
